7905be6b66a685021e97c6f498f06863234fe626
Scheligan <scheliganmonzon1@gmail.com>
2023-09-15 18:04:44 -0300

    OK

---
+++ src/main/java/com/uniamerica/pizzaria/service/SaborService.java
@@ -0,0 +1,13 @@
+package com.uniamerica.pizzaria.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaborService {
+
+    @Transactional
+    public void saveSabor(){
+        
+    }
+}
