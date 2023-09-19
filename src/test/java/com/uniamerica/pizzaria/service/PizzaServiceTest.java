d0c7106ae00c5ea4f3d2a233beb4740cd3476855
Scheligan <scheliganmonzon1@gmail.com>
2023-09-19 10:01:23 -0300

    adicionando mais testes em PizzariaService

--- src/test/java/com/uniamerica/pizzaria/service/PizzaServiceTest.java
+++ src/test/java/com/uniamerica/pizzaria/service/PizzaServiceTest.java
@@ -17,6 +17,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertThrows;
+
 
 @SpringBootTest
 class PizzaServiceTest {
@@ -42,7 +44,7 @@ class PizzaServiceTest {
     }
 
     @Test
-    void save() {
+    void saveP() {
         List<String> ingredientes = new ArrayList<>();
         ingredientes.add("Calabresa");
         Sabor sabor = new Sabor(1L,"Calabresa", ingredientes);
@@ -56,5 +58,94 @@ class PizzaServiceTest {
 
         Assertions.assertNotNull(pizzaservice);
         Assertions.assertEquals(HttpStatus.CREATED, pizzaservice.getStatusCode());
+    }
+    @Test
+    void saveM() {
+        List<String> ingredientes = new ArrayList<>();
+        ingredientes.add("Calabresa");
+        Sabor sabor = new Sabor(1L,"Calabresa", ingredientes);
+        List<String> ingredientes2 = new ArrayList<>();
+        ingredientes.add("Alguma coisa");
+        Sabor sabor2 = new Sabor(2L, "Portuguesa", ingredientes2);
+        List<Sabor> sabores = new ArrayList<>();
+        sabores.add(sabor);
+        sabores.add(sabor2);
+
+        BigDecimal valor = new BigDecimal(25);
+        Pizza pizza = new Pizza(1L, Tamanho.M,sabores,valor);
+
+        var pizzaservice = pizzaService.save(pizza);
+
+        Assertions.assertNotNull(pizzaservice);
+        Assertions.assertEquals(HttpStatus.CREATED, pizzaservice.getStatusCode());
+    }
+    @Test
+    void saveG() {
+        List<String> ingredientes = new ArrayList<>();
+        ingredientes.add("Calabresa");
+        Sabor sabor = new Sabor(1L,"Calabresa", ingredientes);
+        List<String> ingredientes2 = new ArrayList<>();
+        ingredientes.add("Alguma coisa");
+        Sabor sabor2 = new Sabor(2L, "Portuguesa", ingredientes2);
+        List<String> ingredientes3 = new ArrayList<>();
+        ingredientes.add("Frango");
+        Sabor sabor3 = new Sabor(2L, "Strogonoff", ingredientes3);
+        List<Sabor> sabores = new ArrayList<>();
+        sabores.add(sabor);
+        sabores.add(sabor2);
+        sabores.add(sabor3);
+
+        BigDecimal valor = new BigDecimal(25);
+        Pizza pizza = new Pizza(1L, Tamanho.G,sabores,valor);
+
+        var pizzaservice = pizzaService.save(pizza);
+
+        Assertions.assertNotNull(pizzaservice);
+        Assertions.assertEquals(HttpStatus.CREATED, pizzaservice.getStatusCode());
+    }
+    @Test
+    void saveGG() {
+        List<String> ingredientes = new ArrayList<>();
+        ingredientes.add("Calabresa");
+        Sabor sabor = new Sabor(1L,"Calabresa", ingredientes);
+        List<String> ingredientes2 = new ArrayList<>();
+        ingredientes.add("Alguma coisa");
+        Sabor sabor2 = new Sabor(2L, "Portuguesa", ingredientes2);
+        List<String> ingredientes3 = new ArrayList<>();
+        ingredientes.add("Frango");
+        Sabor sabor3 = new Sabor(2L, "Strogonoff", ingredientes3);
+        List<String> ingredientes4 = new ArrayList<>();
+        ingredientes.add("4 queijos");
+        Sabor sabor4 = new Sabor(2L, "4 queijos", ingredientes4);
+
+        List<Sabor> sabores = new ArrayList<>();
+        sabores.add(sabor);
+        sabores.add(sabor2);
+        sabores.add(sabor3);
+        sabores.add(sabor4);
+
+
+        BigDecimal valor = new BigDecimal(25);
+        Pizza pizza = new Pizza(1L, Tamanho.GG,sabores,valor);
+
+        var pizzaservice = pizzaService.save(pizza);
+
+        Assertions.assertNotNull(pizzaservice);
+        Assertions.assertEquals(HttpStatus.CREATED, pizzaservice.getStatusCode());
+    }
+    @Test
+    void erro(){
+
+        List<Sabor> sabores = new ArrayList<>();
+
+        BigDecimal valor = new BigDecimal(25);
+        Pizza pizza = new Pizza(1L, Tamanho.P, sabores, valor);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> pizzaService.save(pizza));
+
+
+        Assertions.assertEquals("insira um sabor", exception.getMessage());
+
+
     }
 }
